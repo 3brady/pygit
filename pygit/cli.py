4df7adc2f04047e5804771bab3b03f1c4c2ce001
ahmed aly abdelrady <22abdelrady22@gmail.com>
2026-08-23 13:40:17 +0300

    added base commands

--- pygit/cli.py
+++ pygit/cli.py
@@ -1,2 +1,45 @@
-def main():
-    print("hello world")
+import argparse , os , sys , pyfiglet
+from . import data , base
+
+def main ():
+    args = parse_args ()
+    args.func (args)
+
+def parse_args ():
+    banner = pyfiglet.figlet_format("PYGIT", font="big")
+
+    parser = argparse.ArgumentParser(prog = 'pygit' , description='a version control system written in python' , epilog=banner , formatter_class=argparse.RawDescriptionHelpFormatter)
+    commands = parser.add_subparsers(dest = "command")
+    commands.required = True
+
+    init_parser = commands.add_parser("init" , help='initialize pygit repository')
+    init_parser.set_defaults(func = init)
+    init_parser.set_defaults(func = init)
+
+    hash_object_parser = commands.add_parser("hash-object" , help='hash pygit object')
+    hash_object_parser.set_defaults(func = hash_object)
+    hash_object_parser.add_argument("file")
+
+    cat_file_parser = commands.add_parser("cat-file" , help='cat file')
+    cat_file_parser.set_defaults(func = cat_file)
+    cat_file_parser.add_argument("object")
+
+    write_tree_parser = commands.add_parser("write-tree" , help='write tree')
+    write_tree_parser.set_defaults(func = write_tree)
+
+    return parser.parse_args()
+
+def init(args):
+    data.init()
+    print(f"Initialized empty pygit repository at {os.getcwd()}/{data.GIT_DIR}")
+
+def hash_object(args):
+    with open(args.file, 'rb') as f :
+        print(data.hash_object(f.read()))
+
+def cat_file(args) :
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
+
+def write_tree(args):
+    print(base.write_tree())
